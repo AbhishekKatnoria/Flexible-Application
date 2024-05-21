415b2fb3e0b17230508c4c575b3587dae72f055f
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2024-05-22 00:26:53 +0530

    Setup the project Created the component for the Navbar and Footer and also created the constent data file also created the common interface type file

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "./globals.css";
+export const metadata = {
+  title: "Application",
+  description: "future Website",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        <main>{children}</main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import { NavLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image src="./logo.svg" width={155} height={50} alt="" />
+        </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map((item, index) => (
+            <Link href={item?.href} key={index}>{item?.text}</Link>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
