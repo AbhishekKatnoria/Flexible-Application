bfaf913454d39237602f85655eb38aab7f0fa1f4
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2024-05-31 22:28:02 +0530

    Added grafbase and footer and page or env file

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      hello
+      <h1></h1>
+      <h1></h1>
+      <h1></h1>
     </div>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,7 +10,6 @@ type column = {
 };
 
 const Footer = () => {
-  console.log(footerLink);
   return (
     <footer className="footer flexStart">
       <div className="flex flex-col w-full gap-12">
@@ -30,18 +29,25 @@ const Footer = () => {
 
         <div className="flex flex-wrap gap-12">
           {footerLink.map((item, index) => (
-              <div className="footer_column">
-                <h4 className="font-semibold">{item?.title}</h4>
-                <ul className="flex flex-col gap-2 font-normal">
-                  {item?.links.map((link, index) => (
-                    <Link href="/" key={index}>
-                      {link}
-                    </Link>
-                  ))}
-                </ul>
-              </div>
+            <div className="footer_column" key={index}>
+              <h4 className="font-semibold">{item?.title}</h4>
+              <ul className="flex flex-col gap-2 font-normal">
+                {item?.links.map((link, index) => (
+                  <Link href="/" key={index} className="hover:bg-red-50">
+                    {link}
+                  </Link>
+                ))}
+              </ul>
+            </div>
           ))}
         </div>
+      </div>
+      <div className="flex flex-col w-full justify-between sm:flex-row text-center">
+        <p>@2022 Flexible All the rights resvered</p>
+        <p className="text-gray">
+          <span className="text-black font-semibold">10,300</span> project
+          submit
+        </p>
       </div>
     </footer>
   );
